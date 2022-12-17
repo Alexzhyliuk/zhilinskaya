5e24861021a3246723b002b2ec517f573f04e0a0
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2022-12-17 18:51:14 +0300

    fix

--- app/settings.py
+++ app/settings.py
@@ -139,7 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = os.environ.get("EMAIL_HOST", 'smtp.mail.ru')
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", 'alexandr-zhyliuk@mail.ru')
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", '6gRKpW0ebz9eaQxvLqJ9')
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", '')
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", 'false') != "True"
 
